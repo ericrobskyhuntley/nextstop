6af67e3083444eed1a74ea41f95b6357ae70d90b
Eric Huntley <ericmhuntley@gmail.com>
2018-11-09 09:57:23 -0500

    implement ECC image alignment

--- CardScanner/scan.py
+++ CardScanner/scan.py
@@ -8,7 +8,9 @@ import cv2
 MAX_FEATURES = 500
 GOOD_MATCH_PERCENT = 0.15
 
-def align_images(im1, im2):
+# ECC as implemented appears to work more consistently on card fronts. Unsure if it's due to parameters or method suitability.
+
+def align_images_orb(im1, im2):
     """
     Feature-based image alignment algorithm that finds homography using the ORB feature detector (Oriented Fast and Rotated Brief).
 
@@ -47,6 +49,41 @@ def align_images(im1, im2):
     im1_reg = cv2.warpPerspective(im1, h, (width, height))
 
     return im1_reg
+
+def align_images_ecc(im1, im2):
+    """
+    Image alignment algorithm using enhanced correlation coefficient method (ECC), which is proposed in: Georgios D. Evangelidis and Emmanouil Z. Psarakis. 2008. "Parametric Image Alignment Using Enhanced Correlation Coefficient Maximization." IEEE Transactions on Pattern Analysis and Machine Intelligence 30 (10): 1-8.
+
+    Code is based on implementation found here: https://www.learnopencv.com/image-alignment-ecc-in-opencv-c-python/
+    """
+    # Convert images to grayscale
+    im1_gray = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+    im2_gray = cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
+    # Find size of image1
+    sz = im1.shape
+    # Define the motion model
+    warp_mode = cv2.MOTION_TRANSLATION
+    # Define 2x3 or 3x3 matrices and initialize the matrix to identity
+    if warp_mode == cv2.MOTION_HOMOGRAPHY :
+        warp_matrix = np.eye(3, 3, dtype=np.float32)
+    else :
+        warp_matrix = np.eye(2, 3, dtype=np.float32)
+    # Specify the number of iterations.
+    number_of_iterations = 5000;
+    # Specify the threshold of the increment
+    # in the correlation coefficient between two iterations
+    termination_eps = 1e-10;
+    # Define termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
+    # Run the ECC algorithm. The results are stored in warp_matrix.
+    (cc, warp_matrix) = cv2.findTransformECC (im1_gray,im2_gray,warp_matrix, warp_mode, criteria)
+    if warp_mode == cv2.MOTION_HOMOGRAPHY :
+        # Use warpPerspective for Homography
+        im2_aligned = cv2.warpPerspective (im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    else :
+        # Use warpAffine for Translation, Euclidean and Affine
+        im2_aligned = cv2.warpAffine(im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    return im2_aligned
 
 def bg_trim(im):
     """
