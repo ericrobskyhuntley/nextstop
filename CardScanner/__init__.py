13628392c898e901b170dae3b3261c6b32ff8252
Eric Huntley <ehuntley@mit.edu>
2018-12-13 13:34:43 -0500

    calibrate to card proofs

--- CardScanner/__init__.py
+++ CardScanner/__init__.py
@@ -19,23 +19,23 @@ MUSEUM_DAY_IN_SECONDS = 49500
 # x, y
 QUESTION_HUES = [
     # 4: My preferred transport mode(s) in 2020 will be...
-    (4, np.array([1, 232, 190])),
+    (4, np.array([1, 170, 180])),
     # 5: In 2040, the average person will...
-    (5, np.array([0, 255, 62])),
+    (5, np.array([176, 130, 83])),
     # Responsibility for autonomous vehicle accidents belongs to...
-    (6, np.array([27, 176, 242])),
+    (6, np.array([24, 130, 230])),
     # In 2040, commuting will take...
-    (7, np.array([60, 135, 150])),
+    (7, np.array([62, 100, 159])),
     # In 2040, everyone will have access to...
-    (8, np.array([107, 255, 42])),
+    (8, np.array([101, 118, 68])),
     # The future of mobility will make the world
-    (9, np.array([142, 192, 70])),
+    (9, np.array([142, 87, 93])),
     # In the future, my transportation costs will...
-    (10, np.array([88, 255, 62])),
+    (10, np.array([76, 126, 106])),
     # Future mobility options will have the greatest imapact on...
-    (12, np.array([174, 156, 208])),
+    (12, np.array([171, 146, 194])),
     # In 2040...
-    (13, np.array([106, 205, 120])),
+    (13, np.array([107, 100, 130])),
     # Travel in the future will be more dangerous for...
-    (14, np.array([13, 250, 243])),
+    (14, np.array([6, 196, 232])),
 ]
